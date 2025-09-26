3ff4192d3eb6a39fabae1e9ab852a81c1efa1de2
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 15:49:35 -0700

    Update header.php

--- hmis/includes/header.php
+++ hmis/includes/header.php
@@ -1,5 +1,5 @@
 <header class="header">
     <span class="menu-toggle" id="menu-toggle"><i class="fa-solid fa-bars"></i></span>
-    <img src="logo.png" alt="InfiHealth Logo" class="logo">
+    <img src="hmis/assets/img/UH.png" alt="InfiHealth Logo" class="logo">
     <h1>InfiHealth HMIS</h1>
 </header>
