b9ad2070ffb6fb883244369d3ac8324bdac8db85
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:39:03 -0700

    Create billing.php

---
+++ hmis/billing.php
@@ -0,0 +1,18 @@
+<?php 
+$activePage = 'billing';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Billing - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+
+    <div class="content">
+        <h1>Billing Module</h1>
+        <p>Here you can process payments and generate invoices.</p>
+    </div>
+</body>
+</html>
