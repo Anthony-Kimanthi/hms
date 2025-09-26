ab4fd3ee99a3affd4d0170bddabe7d3d5c483b0f
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 13:55:22 -0700

    Update sidebar.php

--- hmis/sidebar.php
+++ hmis/sidebar.php
@@ -9,8 +9,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <li><a href="patients.php" class="<?= $current_page == 'patients.php' ? 'active' : '' ?>">👤Patients</a></li>
         <li><a href="doctors.php" class="<?= $current_page == 'doctors.php' ? 'active' : '' ?>">Doctors</a></li>
         <li><a href="billing.php" class="<?= $current_page == 'billing.php' ? 'active' : '' ?>">💳Billing</a></li>
-        <li><a href="triage.php" class="<?= $current_page == 'triage.php' ? 'active' : '' ?>">Triage</a></li>
-        <li><a href="pharmacy.php" class="<?= $current_page == 'pharmacy.php' ? 'active' : '' ?>">Pharmacy</a></li>
+        <li><a href="triage.php" class="<?= $current_page == 'triage.php' ? 'active' : '' ?>">🧑‍⚕️Triage</a></li>
+        <li><a href="pharmacy.php" class="<?= $current_page == 'pharmacy.php' ? 'active' : '' ?>">💊Pharmacy</a></li>
         <li><a href="lab.php" class="<?= $current_page == 'lab.php' ? 'active' : '' ?>">Lab</a></li>
         <li><a href="radiology.php" class="<?= $current_page == 'radiology.php' ? 'active' : '' ?>">Radiology</a></li>
         <li><a href="reports.php" class="<?= $current_page == 'reports.php' ? 'active' : '' ?>">Reports</a></li>
