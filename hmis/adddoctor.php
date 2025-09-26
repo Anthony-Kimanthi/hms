6dced59f879fd5b14cf5f52349b0106d622c4a43
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 10:49:34 -0700

    Update adddoctor.php

--- hmis/adddoctor.php
+++ hmis/adddoctor.php
@@ -1,26 +1,8 @@
 <?php
-include 'db.php'; // include the connection
+include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstname      = $_POST['firstname'];
     $lastname       = $_POST['lastname'];
     $age            = $_POST['age'];
     $specialization = $_POST['specialization'];
-    $timings        = $_POST['timings'];
-
-    $stmt = $conn->prepare("INSERT INTO doctorslist (firstname, lastname, age, specialization, timings) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssiss", $firstname, $lastname, $age, $specialization, $timings);
-
-    if ($stmt->execute()) {
-        header("Location: doctors.php");
-        exit();
-    } else {
-        echo "Error: " . $stmt->error;
-    }
-
-    $stmt->close();
-}
-
-$conn->close();
-?>
-
