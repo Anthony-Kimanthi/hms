2f1cd1360e66f30438bc65ce776aaed7050aa8cf
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:38:39 -0700

    Create patients.php

---
+++ hmis/patients.php
@@ -0,0 +1,18 @@
+<?php 
+$activePage = 'patients';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patients - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+
+    <div class="content">
+        <h1>Patients Module</h1>
+        <p>Here you can register and search patients.</p>
+    </div>
+</body>
+</html>
