5dcdf01e016cb1c0d906961420e277ad2aa4a433
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:40:24 -0700

    Create pharmacy.php

---
+++ hmis/pharmacy.php
@@ -0,0 +1,18 @@
+<?php 
+$activePage = 'pharmacy';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pharmacy - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+
+    <div class="content">
+        <h1>Pharmacy Module</h1>
+        <p>Here you can dispense drugs and manage inventory.</p>
+    </div>
+</body>
+</html>
