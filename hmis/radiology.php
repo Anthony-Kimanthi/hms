039ae28b4c0666da84b19524cb98d00bf36e136b
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 13:35:31 -0700

    Update and rename radiology to radiology.php

---
+++ hmis/radiology.php
@@ -0,0 +1,15 @@
+<?php include 'sidebar.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Radiology - InfiHealth</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main-content">
+        <h1>Radiology Module</h1>
+        <p>Manage radiology tests, imaging records, and reports.</p>
+    </div>
+</body>
+</html>
