3d1d283dbdb70e5142e7a03a19f75d7c2e497a9f
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 10:52:50 -0700

    Update doctors.php

--- hmis/doctors.php
+++ hmis/doctors.php
@@ -1,133 +1,96 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hospital manangement system</title>
-	<!--Favicon-->
-	<link rel="stylesheet" type="text/css" href="css/index.css">
-
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
- <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Scrolling Nav JavaScript -->
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/scrolling-nav.js"></script>
-
+    <title>Hospital Management System</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body id="body">
-	
-
 
 <div id='cssmenu'>
-<ul>
-   <li ><a href='index.php'>Home</a></li>
-
-   <li><a href='about.php'>About</a></li>
-   <li class='active'><a href='doctors.php'>Doctors</a></li>
-   <li><a href='patients.php'>patients</a></li>
-   <label class="heading">Hospital management system</label>
-
-</ul>
+    <ul>
+       <li><a href='index.php'>Home</a></li>
+       <li><a href='about.php'>About</a></li>
+       <li class='active'><a href='doctors.php'>Doctors</a></li>
+       <li><a href='patients.php'>Patients</a></li>
+       <label class="heading">Hospital management system</label>
+    </ul>
 </div>
 
-</body>
-</html>
-   
-    <?php           
+<?php
+include 'db.php';
 
-$db_host="localhost";
-$db_username="root";
-$db_pass="";
-$db_name="hmsdb";
+$result = mysqli_query($conn, "SELECT * FROM doctorslist");
 
-mysql_connect("$db_host","$db_username","$db_pass") or die("error occurred");
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
 
-mysql_select_db("$db_name") or die("no database name");
-echo "<table class='table'>
+echo "<table class='table table-striped'>
  <thead>
       <tr>
-        <th>S.no</th>
-        <th>Name</th>
-        <th>Gender</th>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
         <th>Age</th>
-        <th>mobile</th>
-        <th>Email-id</th>
-        
+        <th>Specialization</th>
+        <th>Timings</th>
       </tr>
-    </thead>";
-$sql1=mysql_query("SELECT * FROM doctorslist");
-$count=mysql_num_rows($sql1);
+    </thead>
+    <tbody>";
 
-for ($i=1; $i <=$count ; $i++) { 
-$query="SELECT * FROM doctorslist WHERE id='$i'";
-$sql=mysql_query($query);
-$fetch=mysql_fetch_array($sql);
-
-if($fetch){
-
-   
-echo "<tbody>";
+while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr>";
-     echo "<td>".$fetch['id']."</td>";
-     echo "<td>".$fetch['firstname']."</td>";
-     echo "<td>".$fetch['lastname']."</td>";
-     echo "<td>".$fetch['age']."</td>";
-     echo "<td>".$fetch['specialization']."</td>";
-     echo "<td>".$fetch['timings']."</td> "  ;    
-    echo "</tr> <br>";
-echo "</tbody>";
-
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['firstname'] . "</td>";
+    echo "<td>" . $row['lastname'] . "</td>";
+    echo "<td>" . $row['age'] . "</td>";
+    echo "<td>" . $row['specialization'] . "</td>";
+    echo "<td>" . $row['timings'] . "</td>";
+    echo "</tr>";
 }
 
-}
-
-echo "</table>";
+echo "</tbody></table>";
 ?>
 
-<center><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Add a new doctor</button></center>
-  <div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
+<center>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+        Add a new doctor
+    </button>
+</center>
 
-    <!-- Modal content-->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Add a doctor</h4>
       </div>
       <div class="modal-body">
-         <form role="form" name="hii" method="post" action="adddoctor.php" class="addform">
-         <div class="form-group">
-            <input type="text" class="form-control" name="firstname" placeholder="Enter the  first name" required>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" name="lastname" placeholder="Enter the  last name" required>
-          </div> 
-           <div class="form-group">
-            <input type="number" class="form-control" name="age" placeholder="Enter the  age " required>
-          </div>
-           <div class="form-group">
-            <input type="text" class="form-control" name="specialization" placeholder="Enter the  specialization" required>
-          </div>
-           <div class="form-group">
-            <input type="text" class="form-control" name="timings" placeholder="Enter the  visiting hours" required>
-          </div>
-        
-      
-      
-        <center><button type="submit" class="btn btn-primary">Submit</button></center>
-          </form>
+         <form role="form" method="post" action="adddoctor.php" class="addform">
+            <div class="form-group">
+                <input type="text" class="form-control" name="firstname" placeholder="Enter the first name" required>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="lastname" placeholder="Enter the last name" required>
+            </div>
+            <div class="form-group">
+                <input type="number" class="form-control" name="age" placeholder="Enter the age" required>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="specialization" placeholder="Enter the specialization" required>
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="timings" placeholder="Enter the visiting hours" required>
+            </div>
+            <center><button type="submit" class="btn btn-primary">Submit</button></center>
+         </form>
       </div>
     </div>
-
   </div>
 </div>
+
+</body>
+</html>
