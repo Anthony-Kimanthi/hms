4111baee271afdc8c48ef7ab243b97dfdd883aa0
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:55:26 -0700

    Create reports.php

---
+++ hmis/reports.php
@@ -0,0 +1,15 @@
+<?php $activePage = 'reports'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reports - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+    <div class="content">
+        <h1>🧾 Reports</h1>
+        <p>Generate and view hospital reports.</p>
+    </div>
+</body>
+</html>
