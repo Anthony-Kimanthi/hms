b795e8b8f5532371af58f398c5539fb9c6d22d27
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:39:32 -0700

    Create triage.php

---
+++ hmis/triage.php
@@ -0,0 +1,18 @@
+<?php 
+$activePage = 'triage';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Triage - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+
+    <div class="content">
+        <h1>Triage Module</h1>
+        <p>Here you can record patient vitals and assign them to doctors.</p>
+    </div>
+</body>
+</html>
