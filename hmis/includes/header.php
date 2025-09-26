1b3714efba4f6286f9ea402eeb22aa2d27a4cae9
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 15:55:39 -0700

    Update header.php

--- hmis/includes/header.php
+++ hmis/includes/header.php
@@ -3,3 +3,23 @@
     <img src="hmis/assets/img/UH.png" alt="InfiHealth Logo" class="logo">
     <h1>InfiHealth HMIS</h1>
 </header>
+----------------------
+<?php
+// Optional: you can pass $pageTitle from each module, fallback to "InfiHealth HMIS"
+if (!isset($pageTitle)) {
+    $pageTitle = "InfiHealth HMIS";
+}
+?>
+
+<div class="header">
+    <!-- Menu toggle for mobile -->
+    <div class="menu-toggle" id="menu-toggle">
+        <i class="fa fa-bars"></i>
+    </div>
+
+    <!-- Logo (if you have one in assets/img/) -->
+    <img src="assets/img/logo.png" alt="InfiHealth Logo" class="logo">
+
+    <!-- Page Title -->
+    <h1><?= htmlspecialchars($pageTitle) ?></h1>
+</div>
