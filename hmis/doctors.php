beb9631f751b3a25b96326c6cad2faa1b9ab8489
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:54:12 -0700

    Create doctors.php

---
+++ hmis/doctors.php
@@ -0,0 +1,15 @@
+<?php $activePage = 'doctors'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doctors - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+    <div class="content">
+        <h1>🩺 Doctors</h1>
+        <p>Here you can manage doctors’ information.</p>
+    </div>
+</body>
+</html>
