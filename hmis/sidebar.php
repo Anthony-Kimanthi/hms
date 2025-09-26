8da16ff37a4c610f795a2a714af42a87c9bb1c3b
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 13:54:39 -0700

    Update sidebar.php

--- hmis/sidebar.php
+++ hmis/sidebar.php
@@ -8,7 +8,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <li><a href="index.php" class="<?= $current_page == 'index.php' ? 'active' : '' ?>">🏥Home</a></li>
         <li><a href="patients.php" class="<?= $current_page == 'patients.php' ? 'active' : '' ?>">👤Patients</a></li>
         <li><a href="doctors.php" class="<?= $current_page == 'doctors.php' ? 'active' : '' ?>">Doctors</a></li>
-        <li><a href="billing.php" class="<?= $current_page == 'billing.php' ? 'active' : '' ?>">Billing</a></li>
+        <li><a href="billing.php" class="<?= $current_page == 'billing.php' ? 'active' : '' ?>">💳Billing</a></li>
         <li><a href="triage.php" class="<?= $current_page == 'triage.php' ? 'active' : '' ?>">Triage</a></li>
         <li><a href="pharmacy.php" class="<?= $current_page == 'pharmacy.php' ? 'active' : '' ?>">Pharmacy</a></li>
         <li><a href="lab.php" class="<?= $current_page == 'lab.php' ? 'active' : '' ?>">Lab</a></li>
