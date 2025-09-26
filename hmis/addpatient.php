3ebeee39ce329f8fb8b0ee708268ba9f9bbcc21c
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 10:47:55 -0700

    Update addpatient.php

--- hmis/addpatient.php
+++ hmis/addpatient.php
@@ -1,70 +1,44 @@
 <?php
-include 'db.php'; // include the connection
+// db.php already has the connection logic, so let’s use it
+include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name    = $_POST['name'];
-    $gender  = $_POST['gender'];
-    $age     = $_POST['age'];
-    $mobile  = $_POST['mobile'];
-    $email   = $_POST['email'];
-    $address = $_POST['address'];
+    $name   = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age    = $_POST['age'];
+    $mobile = $_POST['mobile'];
+    $email  = $_POST['email'];
+    $address= $_POST['address'];
 
-    $stmt = $conn->prepare("INSERT INTO addpatient (name, gender, age, mobile, email, address) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssisss", $name, $gender, $age, $mobile, $email, $address);
+    $query = "INSERT INTO addpatient (name, gender, age, mobile, email, address)
+              VALUES ('$name', '$gender', '$age', '$mobile', '$email', '$address')";
 
-    if ($stmt->execute()) {
-        $message = "Patient added successfully.";
+    if (mysqli_query($conn, $query)) {
+        // Redirect to patients list after success
+        header("Location: patients.php");
+        exit();
     } else {
-        $message = "Error: " . $stmt->error;
+        echo "Error: " . mysqli_error($conn);
     }
-
-    $stmt->close();
 }
-
-$conn->close();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Hospital Management System</title>
-    <!--Favicon-->
     <link rel="stylesheet" type="text/css" href="css/index.css">
-
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Scrolling Nav JavaScript -->
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/scrolling-nav.js"></script>
 </head>
 <body id="body">
-
 <div id='cssmenu'>
     <ul>
-        <li><a href='index.php'>Home</a></li>
-        <li><a href='about.php'>About</a></li>
-        <li><a href='doctors.php'>Doctors</a></li>
-        <li><a href='patients.php'>Patients</a></li>
+       <li><a href='index.php'>Home</a></li>
+       <li><a href='about.php'>About</a></li>
+       <li><a href='doctors.php'>Doctors</a></li>
+       <li><a href='patients.php'>Patients</a></li>
     </ul>
 </div>
-
-<div class="container">
-    <h3><?php echo isset($message) ? $message : "Fill the form to add a patient."; ?></h3>
-</div>
-
 </body>
 </html>
-
