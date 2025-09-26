2dfba851c5a1d8e5905374f23a3417d8d93e1203
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-26 11:57:20 -0700

    Create admin.php

---
+++ hmis/admin.php
@@ -0,0 +1,15 @@
+<?php $activePage = 'admin'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin - HMS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+    <div class="content">
+        <h1>🔑 Admin</h1>
+        <p>Admin settings and role management.</p>
+    </div>
+</body>
+</html>
