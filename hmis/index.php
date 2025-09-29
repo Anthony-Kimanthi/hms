889d7e447417d3bbd02a789a23ce42240236c815
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-29 10:46:28 -0700

    Update index.php

--- hmis/index.php
+++ hmis/index.php
@@ -10,13 +10,12 @@ $pageDescription = "Welcome to InfiHealth MIS"; // e.g., "Process payments and g
 <head>
     <meta charset="UTF-8">
     <title><?= $pageTitle ?> - InfiHealth HMIS</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
-    <?php include '../includes/sidebar.php'; ?>
-    <?php include '../includes/header.php'; ?>
-
+    <?php include __DIR__ . '/includes/sidebar.php';?> 
+<?php include __DIR__ . '/includes/header.php';?> 
     <div class="content with-header">
         <h1><?= $pageHeader ?></h1>
         <p><?= $pageDescription ?></p>
@@ -25,9 +24,10 @@ $pageDescription = "Welcome to InfiHealth MIS"; // e.g., "Process payments and g
         <!-- Example: table of patients, billing list, doctor schedules, etc. -->
     </div>
 
-    <script src="../js/script.js"></script>
+    <script src="./js/script.js"></script>
 </body>
 </html>
+
 
 
 
