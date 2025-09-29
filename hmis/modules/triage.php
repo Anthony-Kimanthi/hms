6a0b505e14d2a8671f43b57ab64c679eaa9ae6c8
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-29 11:50:40 -0700

    Update triage.php

--- hmis/modules/triage.php
+++ hmis/modules/triage.php
@@ -10,13 +10,14 @@ $pageDescription = "OP Nurse examination modules, vital recordings, HOPI etc."; 
 <head>
     <meta charset="UTF-8">
     <title><?= $pageTitle ?> - InfiHealth HMIS</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
-    <?php include __DIR__ . '/includes/sidebar.php';?> 
-    <?php include __DIR__ . '/includes/header.php';?> 
-
+   <?php
+include("../includes/sidebar.php");
+include("../includes/header.php");
+?>
     <div class="content with-header">
         <h1><?= $pageHeader ?></h1>
         <p><?= $pageDescription ?></p>
@@ -25,6 +26,6 @@ $pageDescription = "OP Nurse examination modules, vital recordings, HOPI etc."; 
         <!-- Example: table of patients, billing list, doctor schedules, etc. -->
     </div>
 
-    <script src="./js/script.js"></script>
+    <script src="../js/script.js"></script>
 </body>
 </html>
