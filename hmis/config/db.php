672391ec8f94ac45cc99bbfb86b8544d27f093e2
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-29 10:21:32 -0700

    Update db.php

--- hmis/config/db.php
+++ hmis/config/db.php
@@ -1,1 +1,13 @@
+<?php
+$host = "db";          // service name from docker-compose.yml
+$user = "hmis_user";   // set in environment
+$pass = "hmis_pass";   // set in environment
+$dbname = "hmis_db";   // set in environment
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
