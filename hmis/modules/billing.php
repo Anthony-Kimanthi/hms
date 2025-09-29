1018a475fedbd8ce3109345e89b0bde0591aa161
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-29 11:45:14 -0700

    Update billing.php

--- hmis/modules/billing.php
+++ hmis/modules/billing.php
@@ -10,12 +10,16 @@ $pageDescription = "Here you can process payments and generate invoices."; // e.
 <head>
     <meta charset="UTF-8">
     <title><?= $pageTitle ?> - InfiHealth HMIS</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
-        <?php include __DIR__ . '/../includes/sidebar.php';?> 
-        <?php include __DIR__ . '/../includes/header.php';?> 
+
+<?php
+include("../includes/sidebar.php");
+include("../includes/header.php");
+?>
+
 
     <div class="content with-header">
         <h1><?= $pageHeader ?></h1>
