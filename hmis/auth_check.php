2c1c13eadb6ca087407e92c07640555e283407af
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-10-07 15:53:20 -0700

    Update auth_check.php

--- hmis/auth_check.php
+++ hmis/auth_check.php
@@ -1,16 +1,8 @@
 <?php
 session_start();
 
-// Redirect to login if not logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
 
-// Check role access (optional per module)
-function checkRole($allowedRoles) {
-    if (!in_array($_SESSION['role'], $allowedRoles)) {
-        header("Location: unauthorized.php");
-        exit;
-    }
-}
