011afd910d35f575b4b219716569834e1e8822bf
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-10-09 12:22:02 -0700

    Update sidebar.php

--- hmis/includes/sidebar.php
+++ hmis/includes/sidebar.php
@@ -18,14 +18,14 @@ function isActive($path) {
     </div>
 
     <ul class="sidebar-menu">
-        <li class="<?php echo isActive('/modules/admin/'); ?>">
-            <a href="/modules/admin/dashboard.php"><i class="fas fa-user-shield"></i> Admin</a>
+        <li class="<?php echo isActive('/modules/appointments/'); ?>">
+            <a href="/modules/appointments/dashboard.php"><i class="fas fa-calendar-check"></i> Appointments</a>
+        </li>
+        <li class="<?php echo isActive('/modules/patients/'); ?>">
+            <a href="/modules/patients/dashboard.php"><i class="fas fa-users"></i> Patients</a>
         </li>
         <li class="<?php echo isActive('/modules/billing/'); ?>">
             <a href="/modules/billing/dashboard.php"><i class="fas fa-file-invoice-dollar"></i> Billing</a>
-        </li>
-        <li class="<?php echo isActive('/modules/pharmacy/'); ?>">
-            <a href="/modules/pharmacy/dashboard.php"><i class="fas fa-pills"></i> Pharmacy</a>
         </li>
         <li class="<?php echo isActive('/modules/laboratory/'); ?>">
             <a href="/modules/laboratory/dashboard.php"><i class="fas fa-vial"></i> Laboratory</a>
@@ -33,18 +33,17 @@ function isActive($path) {
         <li class="<?php echo isActive('/modules/radiology/'); ?>">
             <a href="/modules/radiology/dashboard.php"><i class="fas fa-x-ray"></i> Radiology</a>
         </li>
+        <li class="<?php echo isActive('/modules/pharmacy/'); ?>">
+            <a href="/modules/pharmacy/dashboard.php"><i class="fas fa-pills"></i> Pharmacy</a>
+        </li>
+        
         <li class="<?php echo isActive('/modules/reports/'); ?>">
             <a href="/modules/reports/dashboard.php"><i class="fas fa-chart-line"></i> Reports</a>
         </li>
-        <li class="<?php echo isActive('/modules/inventory/'); ?>">
-            <a href="/modules/inventory/dashboard.php"><i class="fas fa-boxes"></i> Inventory</a>
+        <li class="<?php echo isActive('/modules/admin/'); ?>">
+            <a href="/modules/admin/dashboard.php"><i class="fas fa-user-shield"></i> Admin</a>
         </li>
-        <li class="<?php echo isActive('/modules/patients/'); ?>">
-            <a href="/modules/patients/dashboard.php"><i class="fas fa-users"></i> Patients</a>
-        </li>
-        <li class="<?php echo isActive('/modules/appointments/'); ?>">
-            <a href="/modules/appointments/dashboard.php"><i class="fas fa-calendar-check"></i> Appointments</a>
-        </li>
+        
     </ul>
 
     <div class="sidebar-footer">
